82bf5f46cdfbf40c33ce416f180a82ce6460dcf2
Asma Ahmed <asmaahmed0310@gmail.com>
2023-09-25 06:32:44 +0000

    first commit

---
+++ fileprocessor2.py
@@ -0,0 +1,6 @@
+import sys
+
+for line in sys.stdin:
+    if not line.startswith('#'):
+        username, password, userid, groupid = line.strip().split(':')
+        print(f"The user {username} has a password of {password} and has userid {userid} and groupid {groupid}")
